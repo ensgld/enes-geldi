e46e24647ca5efa171e14be2e17fdeae29cb90a4
Enes Geldi <121521045+ensgld@users.noreply.github.com>
2024-08-20 14:58:45 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+Bizim aslında bütün uygulamalarımız bir package olarak saklanır.
+
+Package oluşturma komutu: npm init
+package oluşturduktan sonra enter deyip 
+description: 
+entry point keywords gibi yerleri doldurduktan sonra package oluşur
+
+sonrasında package.json adında bir package dosyası gelir ve buradan sonrasında istediğimiz değişiklikleri yapabiliriz
+scriptteki değiştirmek istediğimiz de "echo 'Bu benim adım'" gibi değiştirip scriptte de kaydedersek
+değiştirdiğimiz şeyi de çalıştırmak istersek npm run enes gibi çalıştırabiliriz 
+
+bir package'ı sadece geliştirme ortamında kullanmak için indireceksek npm install (package adı) --save-dev
+
+*/ 
+console.log("Bu benim uygulamam...");
+
+
+
